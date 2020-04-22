6b0391f65db3bdd901b979aeb3757c8082b369a5
Daniel Stack <dstack1776@gmail.com>
2020-04-21 21:04:57 -0400

    unit test

--- mbta.py
+++ mbta.py
@@ -1,20 +1,33 @@
 import requests
 import yaml
 import sys
+from enum import Enum
+
+class MbtaErrorCodes(Enum):
+    Success = 0
+    NoOutput = 1
+    Non200Resp = 2
+    NoArgs = 3
+    BadArgs = 4
 
 def print_all_lines(get_routes_url):
+    rc = MbtaErrorCodes.Success
     # adjust the payload to only have what we need, sort it by id
     payload = {'filter[type]': '0,1', 'fields[route]': 'long_name,id', 'sort': 'id'}
     resp = requests.get(get_routes_url, payload)
     if resp.status_code != 200:
         # This means something went wrong.
         print('Problem getting data from MBTA. Please try again or contact support.')
-        return
+        return MbtaErrorCodes.Non200Resp
+
     lines_json_dict = resp.json()
+    if len(lines_json_dict) == 0:
+        return MbtaErrorCodes.NoOutput
     for curr_line in lines_json_dict['data']:
         line_id = curr_line['id']
         line_name = curr_line['attributes']['long_name']
         print('ID: {}, NAME: {}'.format(line_id, line_name))
+    return MbtaErrorCodes.Success
 
 def print_stops(get_stops_url, line_id):
     # adjust the payload to only have what we need, filter by line. Do not sort as data is in correct order
@@ -22,18 +35,23 @@ def print_stops(get_stops_url, line_id):
     payload = {'filter[route]': line_id, 'fields[stop]': 'name'}
 
     resp = requests.get(get_stops_url, payload)
+
     if resp.status_code != 200:
         # This means something went wrong.
         print('Problem getting data from MBTA. Please try again or contact support.')
-        return
+        return MbtaErrorCodes.Non200Resp
+
     stops_json_dict = resp.json()
     if len(stops_json_dict['data']) == 0:
         print('No stops found for line ID \'{}\'. Please verify the line ID and remember they are case-sensitive'.format(line_id))
-    else:
-        for curr_stop in stops_json_dict['data']:
-            stop_name = curr_stop['attributes']['name']
-            print(stop_name)
-    return len(stops_json_dict['data'])
+        return MbtaErrorCodes.NoOutput
+
+    for curr_stop in stops_json_dict['data']:
+        stop_name = curr_stop['attributes']['name']
+        print(stop_name)
+
+    return MbtaErrorCodes.Success
+
 
 def usage_message(custom_message=None):
     """
@@ -55,21 +73,25 @@ def main(args):
 
     if len(args) < 2:
         usage_message('At least one option required')
+        return MbtaErrorCodes.NoArgs
+    curr_arg = args[1]
+    if curr_arg == '--get-routes':
+        if len(args) != 2:
+            usage_message('--get-routes has no parameters')
+            return MbtaErrorCodes.BadArgs
+        return print_all_lines(api_dict['get_routes'])
+    elif curr_arg == '--get-stops':
+        if len(args) != 3:
+            usage_message('--get-stops requires a single parameter')
+            return MbtaErrorCodes.BadArgs
+        line_id = args[2]
+        return print_stops(api_dict['get_stops'], line_id)
+    elif curr_arg == '--help':
+        usage_message()
+        return MbtaErrorCodes.Success
     else:
-        curr_arg = args[1]
-        if curr_arg == '--get-routes':
-            if len(args) != 2:
-                usage_message('--get-routes has no parameters')
-            else:
-                print_all_lines(api_dict['get_routes'])
-        elif curr_arg == '--get-stops':
-            if len(args) != 3:
-                usage_message('--get-stops requires a single parameter')
-            else:
-                line_id = args[2]
-                print_stops(api_dict['get_stops'], line_id)
-        else:
-            usage_message()
+        usage_message()
+        return MbtaErrorCodes.BadArgs
 
 
 
--- mbta_unit_test.py
+++ mbta_unit_test.py
@@ -1,10 +1,73 @@
 import unittest
+from mbta import MbtaErrorCodes, print_all_lines, print_stops, main
+import yaml
 
+class MbtaUnitTest(unittest.TestCase):
+    def setUp(self):
+        with open(r'mbta.yaml') as file:
+            # The FullLoader parameter handles the conversion from YAML
+            # scalar values to Python the dictionary format
+            self.api_dict = yaml.load(file, Loader=yaml.FullLoader)
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_get_routes(self):
+        rc = print_all_lines(self.api_dict['get_routes'])
+        self.assertEqual(MbtaErrorCodes.Success, rc)
 
+    def test_get_routes_bad_url(self):
+        rc = print_all_lines(self.api_dict['get_routes']+'foo')
+        self.assertEqual(MbtaErrorCodes.Non200Resp, rc)
+
+    def test_get_stops(self):
+        rc = print_stops(self.api_dict['get_stops'], 'Red')
+        self.assertEqual(MbtaErrorCodes.Success, rc)
+
+    def test_get_routes_bad_url(self):
+        rc = print_stops(self.api_dict['get_stops']+'foo', 'Red')
+        self.assertEqual(MbtaErrorCodes.Non200Resp, rc)
+
+    def test_get_routes_bad_line(self):
+        rc = print_stops(self.api_dict['get_stops'], 'red')
+        self.assertEqual(MbtaErrorCodes.NoOutput, rc)
+
+    def test_main_get_all_routes(self):
+        args = ['', '--get-routes']
+        rc = main(args)
+        self.assertEqual(MbtaErrorCodes.Success, rc)
+
+    def test_main_get_all_routes_extra_param(self):
+        args = ['', '--get-routes', 'Red']
+        rc = main(args)
+        self.assertEqual(MbtaErrorCodes.BadArgs, rc)
+
+    def test_main_get_stops(self):
+        args = ['', '--get-stops', 'Green-B']
+        rc = main(args)
+        self.assertEqual(MbtaErrorCodes.Success, rc)
+
+    def test_main_get_stops_missing_param(self):
+        args = ['', '--get-stops']
+        rc = main(args)
+        self.assertEqual(MbtaErrorCodes.BadArgs, rc)
+
+    def test_main_get_stops_extra_param(self):
+        args = ['', '--get-stops', 'Green-B', 'Green-C']
+        rc = main(args)
+        self.assertEqual(MbtaErrorCodes.BadArgs, rc)
+
+    def test_main_no_args(self):
+        args = ['']
+        rc = main(args)
+        self.assertEqual(MbtaErrorCodes.NoArgs, rc)
+
+    def test_main_bad_args(self):
+        args = ['', '--get-stoops']
+        rc = main(args)
+        self.assertEqual(MbtaErrorCodes.BadArgs, rc)
+
+    def test_main_help(self):
+        args = ['', '--help']
+        rc = main(args)
+        self.assertEqual(MbtaErrorCodes.Success, rc)
 
 if __name__ == '__main__':
     unittest.main()
